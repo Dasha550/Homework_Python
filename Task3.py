a28c559ce2c69c3690d547b0932cccef473aa791
Dasha <dashan340@gmail.com>
2022-05-29 01:17:18 +0300

    Третья задача

---
+++ Task3.py
@@ -0,0 +1,15 @@
+#В заданном списке вещественных чисел найдите разницу между максимальным и минимальным значением 
+#дробной части элементов. 
+#Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+from array import array
+import numbers
+
+numbers=[1.1, 1.2, 3.1, 5, 10.01]
+for i in range(len(numbers)):
+    numbers[i]=(round((numbers[i]- int (numbers[i])),2))
+unnecessary_index = numbers.index(0)
+numbers.pop(unnecessary_index)
+        
+max_floor = max(numbers)
+min_floor = min(numbers)
+print(max_floor-min_floor)
